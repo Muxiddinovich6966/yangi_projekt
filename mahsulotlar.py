0ea4a5ceddbb57837cc1ae4a50cb6fcb19f5329f
Farruxbek <norpolatovfarruxbek0@gmail.com>
2024-12-21 19:01:02 +0500

    Add files via upload

---
+++ mahsulotlar.py
@@ -0,0 +1,22 @@
+"sanjar"
+
+class Mahsulotlar:
+    def __init__(self, nomi, narx, muddat):
+        self.nomi = nomi
+        self.narx = narx
+        self.muddat = muddat
+        self.soni = 100
+
+
+tavarlar = [
+    Mahsulotlar("kartoshka", 9000, "02/02/2025"),
+    Mahsulotlar("fanta", 15000, "02/06/2025"),
+    Mahsulotlar("go`sht", 110000, "02/01/2025"),
+    Mahsulotlar("piyoz", 4500, "09/02/2025"),
+    Mahsulotlar("sabzi", 3000, "02/01/2025"),
+    Mahsulotlar("sok", 12000, "02/07/2025"),
+    Mahsulotlar("cola", 15000, "02/09/2025"),
+    Mahsulotlar("shokalad", 30000, "02/12/2025"),
+    Mahsulotlar("guruch", 25000, "02/01/2026"),
+    Mahsulotlar("shakar", 14000, "02/03/2026"),
+]
